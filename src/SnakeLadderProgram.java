586754313a026a49f413db38b8f967808f6582d0
Admin <sandhyabagale10@gmail.com>
2021-09-17 16:36:42 +0530

    rolls the die to get number from 1 to 6

--- src/SnakeLadderProgram.java
+++ src/SnakeLadderProgram.java
@@ -1,10 +1,15 @@
+import java.util.Random;
 
 public class SnakeLadderProgram {
 
 	public static void main(String[] args) {
 
-		int position=0;
+		
 		System.out.println("Snake ladder program played with plyaer 1 ");
+		int position=0;
+		Random rand = new Random();
+		int num= rand.nextInt(7);
+		System.out.println("Random number :" +num);
 	}
 
 }
